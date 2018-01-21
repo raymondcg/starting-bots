9d5ddcdd586098ad0fc541a94f45e16a1bd2fa3f
Raymond Gillett <raymondcgillett@gmail.com>
2018-01-21 13:45:34 -0500

    adding equipment

--- src/control.lua
+++ src/control.lua
@@ -3,7 +3,19 @@ script.on_event(defines.events.on_player_created, function(event)
 	
 	player.insert{name = "construction-robot", count = 20}
 	player.insert{name = "modular-armor", count = 1}
-	player.insert{name = "personal-roboport-equipment", count = 1}
-	player.insert{name = "solar-panel-equipment", count = 15}
+	player.insert{name = "personal-roboport-equipment", count = 4}
+	player.insert{name = "portable-fusion-reactor", count = 1}
+	player.insert{name = "nightvision", count = 1}
+	-- player.insert{name = "solar-panel-equipment", count = 15}
 	player.insert{name = "battery-equipment", count = 3}
 end)
+
+
+--01234567
+--1xxxxnnb
+--2xxxxnnb
+--3xxxxrrb
+--4xxxxrrb
+--5rrrrrrb
+--6rrrrrrb
+--7
