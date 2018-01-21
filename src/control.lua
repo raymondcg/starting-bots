ffd755130613c4a592741a37534ca6b250ae128d
Raymond Gillett <raymondcgillett@gmail.com>
2018-01-21 14:53:18 -0500

    Updating

--- src/control.lua
+++ src/control.lua
@@ -2,10 +2,10 @@ script.on_event(defines.events.on_player_created, function(event)
 	local player = game.players[event.player_index]
 	
 	player.insert{name = "construction-robot", count = 20}
-	player.insert{name = "modular-armor", count = 1}
+	player.insert{name = "power-armor", count = 1}
 	player.insert{name = "personal-roboport-equipment", count = 4}
-	player.insert{name = "portable-fusion-reactor", count = 1}
-	player.insert{name = "nightvision", count = 1}
+	player.insert{name = "fusion-reactor-equipment", count = 1}
+	player.insert{name = "night-vision-equipment", count = 1}
 	-- player.insert{name = "solar-panel-equipment", count = 15}
 	player.insert{name = "battery-equipment", count = 3}
 end)
